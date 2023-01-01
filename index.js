652a8f06bbe25305011838ff498acc83125fdde1
Ahmar khan <ahmarbilal223344@gmail.com>
2023-01-01 18:14:43 +0500

    done

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
+const inp = document.getElementById("typing");
+const quotDisplay = document.getElementById("texts");
+
+const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
+
 async function gettingText() {
-  const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
-  const response = await fetch(RANDOM_QUOTE_API_URL);
-  const data = await response.json();
-  return (text = data.content);
+  const fetched = await fetch(RANDOM_QUOTE_API_URL);
+  const response = await fetched.json();
+  const text = response.content;
+  console.log(text);
+  let splitText = text.split("");
+  splitText.forEach((char) => {
+    const spanEl = document.createElement("span");
+    spanEl.innerText = char;
+    quotDisplay.appendChild(spanEl);
+  });
+
+  cheching();
 }
 
-const inp = document.getElementById("typing");
-const quoteDisplayElement = document.getElementById("texts");
+gettingText();
 
-let splitText = text.split("");
-console.log(splitText);
-splitText.forEach((char) => {
-  let spanEl = document.createElement("span");
-  spanEl.innerText = char;
-  quoteDisplayElement.appendChild(spanEl);
-});
+function cheching() {
+  inp.addEventListener("input", () => {
+    const quotArray = quotDisplay.querySelectorAll("span");
+    const inpArray = inp.value.split("");
+    let correct = true;
+    quotArray.forEach((charSpan, index) => {
+      const char = inpArray[index];
+      if (char == null) {
+        charSpan.classList.remove("correct");
+        charSpan.classList.remove("incorrect");
+        correct = false;
+      } else if (char === charSpan.innerText) {
+        charSpan.classList.add("correct");
+        charSpan.classList.remove("incorrect");
+      } else {
+        charSpan.classList.remove("correct");
+        charSpan.classList.add("incorrect");
+        correct = false;
+      }
+    });
 
-console.log(quoteDisplayElement);
-
-inp.addEventListener("input", (e) => {
-  const arrayQuote = quoteDisplayElement.querySelectorAll("span");
-  const arrayValue = inp.value.split("");
-
-  console.log(arrayQuote);
-  console.log(arrayValue);
-
-  let correct = true;
-  arrayQuote.forEach((charSpan, index) => {
-    const char = arrayValue[index];
-    if (char == null) {
-      charSpan.classList.remove("correct");
-      charSpan.classList.remove("incorrect");
-      correct = false;
-    } else if (char == charSpan.innerText) {
-      charSpan.classList.add("correct");
-      charSpan.classList.remove("incorrect");
-    } else {
-      charSpan.classList.remove("correct");
-      charSpan.classList.add("incorrect");
-      correct = false;
+    if (correct) {
+      inp.value = "";
+      quotDisplay.innerHTML = "";
+      gettingText();
     }
   });
-  if (correct) {
-    inp.value = "";
-  }
-});
+}
